3483cbeae72b54374cabfe376254822b4495b8af
tekhin <tekhin@cvisionlab.com>
2023-11-03 19:13:11 +0300

    feat: initial commit

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def readme():
-  with open('README.md', 'r') as f:
-    return f.read()
-
-setup(
-  name='MedicalImageConversion',
-  version='1.0.0',
-  author='Vasiliy Tekhin',
-  author_email='tekhinvasiliy@gmail.com',
-  description='Convert medical images',
-  long_description=readme(),
-  long_description_content_type='text/markdown',
-  url='https://github.com/Constanter/MedicalImageConversion',
-  packages=find_packages(),
-  install_requires=['requests>=2.25.1'],
-  classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-  ],
-  keywords='example python',
-  project_urls={
-    'Documentation': 'link'
-  },
-  python_requires='>=3.7'
-)
+setup(name='MedicalImageConversion',
+      version='1.0',
+      description='Medical image conversion',
+      packages=['MedicalImageConversion'],
+      author_email='tekhinvasiliy@gmail.com',
+      zip_safe=False)
